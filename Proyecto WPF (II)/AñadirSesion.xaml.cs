bff82d1b5f5e9a434eb52b0b643e9c315f22ab2b
Daniel Alonso <de.alonso@iesdoctorbalmis.com>
2021-02-22 18:36:44 +0100

    Ya no más wey

---
+++ Proyecto WPF (II)/AñadirSesion.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_WPF__II_
+{
+    public partial class ConfiguracionSalas : Window
+    {
+        public int IdPelicula { get; set; }
+        public int IdSala { get; set; }
+        public string Hora { get; set; }
+        public ObservableCollection<Pelicula> ListaPeliculas { get; set; }
+        public ObservableCollection<Sala> Salas { get; set; }
+        public List<string> Horarios { get; set; }
+        public ConfiguracionSalas()
+        {
+            InitializeComponent();
+            peliculasComboBox.ItemsSource = ListaPeliculas;
+            salasComboBox.ItemsSource = Salas;
+            horaComboBox.ItemsSource = Horarios;
+            DataContext = this;
+        }
+        private void aceptarButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
--- Proyecto WPF (II)/MainWindow.xaml.cs
+++ Proyecto WPF (II)/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Proyecto_WPF__II_
             _vistaModelo = new MainWindowVM();
             this.DataContext = _vistaModelo;
             //Obtendremos las sesiones por la película seleccionada
-            if(peliculasListBox.SelectedValue != null)
+            if (peliculasListBox.SelectedValue != null)
             {
                 _vistaModelo.SesionesPorPelicula = _vistaModelo.ObtenerSesionesPorPelicula(_vistaModelo.PeliculaSeleccionada.Id);
             }
@@ -33,12 +33,24 @@ namespace Proyecto_WPF__II_
 
         private void CommandBinding_Executed_Config(object sender, ExecutedRoutedEventArgs e)
         {
-            ConfiguracionSalas config = new ConfiguracionSalas(_vistaModelo);
+            ConfiguracionSalas config = new ConfiguracionSalas();
             config.Owner = this;
+            config.ListaPeliculas = _vistaModelo.ListaPeliculas;
+            config.Salas = _vistaModelo.Salas;
+            config.Horarios = _vistaModelo.Horarios;
 
-            if(config.ShowDialog() == true)
+            if (config.ShowDialog() == true)
             {
-                
+                _vistaModelo.AñadirSesion(config.IdPelicula + 1, config.IdSala + 1, config.Hora);
+            }
+        }
+        private void CommandBinding_Executed_Add(object sender, ExecutedRoutedEventArgs e)
+        {
+            CapacidadSala capacidadSala = new CapacidadSala();
+            capacidadSala.Owner = this;
+            if (capacidadSala.ShowDialog() == true)
+            {
+                _vistaModelo.AñadirSala(capacidadSala.Capacidad);
             }
         }
     }
